d7d26aee0cee99216e79c8aebd06b038506e9f48
RiskaElFauzi <47841739+RiskaElFauzi@users.noreply.github.com>
2019-03-20 10:18:39 +0700

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,13 @@
+var myRequest = new XMLHttpRequest();
+myRequest.open('GET', 'https://codepen.io/eclairereese/pen/BzQBzR.html');
+myRequest.onreadystatechange = function () {
+	// body...
+	if (myRequest.readystate === 4) {
+		document.getElementById('ajax-content').innerHTML = myRequest.responseText;
+	}
+};
+function SendTheAJX(){
+	myRequest.send();
+	document.getElementById('reveal').style.display = 'none';
+	
+}
